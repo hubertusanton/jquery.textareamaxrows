dc893c0fddd15eb76189271391b018c6c1841121
Bart van Irsel <bart@30.nl>
2012-09-25 17:53:51 +0200

    index.html edited

--- custom.js
+++ custom.js
@@ -2,8 +2,6 @@
 $(document).ready(function() {
     
     $('#idoftextarea').textareamaxrows();
-    $('#idoftextarea').textareamaxrows({alert:true});
-    $('#idoftextarea').textareamaxrows({alert:true, alertmessage : 'hey too long!', maxrows : 4, maxcharsinrow : 12});
     
 });    
     
