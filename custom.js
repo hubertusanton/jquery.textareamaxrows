ddc014b0664f4742ffa027567b0ad61739a84300
Bart van Irsel <bart@30.nl>
2012-10-01 13:18:13 +0200

    added options and fixed bugs

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function() {
     
-    $('#idoftextarea').textareamaxrows({alert:true, alertmessage : 'hey too long!', maxrows : 4, maxcharsinrow : 12});
-    
+    //$('#idoftextarea').textareamaxrows({alert:true, alertmessage : 'hey too long!', maxrows : 4, maxcharsinrow : 12});
+    //$('#idoftextarea').textareamaxrows({maxrows : 4, maxcharsinrow : 12});
+    $('#idoftextarea').textareamaxrows({usecounter : true, counterelem : '#counter'});
 });    
     
--- jquery.textareamaxrows.js
+++ jquery.textareamaxrows.js
@@ -1,9 +1,20 @@
 
 
 (function($) {
+    
     $.fn.textareamaxrows = function(options) {
     
-        var opts = $.extend({}, $.fn.textareamaxrows.defaults, options);
+        var defaults = {
+            alert: false, 
+            alertmessage : 'too many rows and chars', 
+            maxrows : 20, 
+            maxcharsinrow : 32,
+            usecounter : false,
+            counterelem : '',
+            errorclass : 'error'
+        }    
+    
+        var opts = $.extend(defaults, options);
 
         return this.each(function() {
             $(this).keyup(onKeyup);
@@ -11,7 +22,6 @@
 
         function onKeyup() {
 
-            var textarea_content = $(this).val();
             var number_breaks    = $(this).val().split('\n').length; 
             // first break does not count
             number_breaks--;
@@ -24,44 +34,59 @@
                     number_fake_breaks = number_fake_breaks + number_fake_breaks_paragraph;
                 }
             }
-			
-            if ((number_breaks + number_fake_breaks) >= opts.maxrows) {
+            
+            var total_breaks = parseInt(number_breaks + number_fake_breaks);
+            
+            if (total_breaks >= opts.maxrows) {
                 if (opts.alert) {
                     alert(opts.alertmessage);
                 }
-                $(this).val(textarea_content.slice(0, -1));
-            }        
+                
+                // add error class to textarea
+                $(this).addClass(opts.errorclass);
+
+                // Cut down the string
+                var current_length = $(this).val().length;
+                var new_length = current_length - 1;
+                $(this).val($(this).val().substr(0, new_length));
+            } 
+            else {
+                // remove error class textarea
+                $(this).removeClass(opts.errorclass);                
+            }
+            
+            // set counter if option usecounter and counterelem and is set
+            if ((opts.usecounter) && opts.counterelem != '') {
+                if ((opts.maxrows - total_breaks) < 0) {
+                    $(opts.counterelem).html('0');
+                }
+                else {
+                    $(opts.counterelem).html(opts.maxrows - total_breaks);
+                }
+            }
 
             return false;
         }
-    
-    
-    }
+        
+        
+        function getNumberOfChunks(chunkSize, checkString) {
 
-    function getNumberOfChunks(chunkSize, checkString) {
+            var chunks = [];
 
-        var chunks = [];
-
-        while (checkString) {
-            if (checkString.length < chunkSize) {
-                chunks.push(checkString);
-                break;
+            while (checkString) {
+                if (checkString.length < chunkSize) {
+                    chunks.push(checkString);
+                    break;
+                }
+                else {
+                    chunks.push(checkString.substr(0, chunkSize));
+                    checkString = checkString.substr(chunkSize);
+                }
             }
-            else {
-                chunks.push(checkString.substr(0, chunkSize));
-                checkString = checkString.substr(chunkSize);
-            }
-        }
-		
-        return chunks.length;
-    }
 
-    $.fn.textareamaxrows.defaults = {
-        alert: false, 
-        alertmessage : 'too rows and chars', 
-        maxrows : 20, 
-        maxcharsinrow : 32
-    }
+            return chunks.length;
+        }     
     
+    }
 
 })(jQuery);
